3a4fb201961b1e3ea53a095095e82700f1ee5687
Aditya Joshi <jaditya1901@yahoo.com.sg>
2023-01-16 12:47:18 +0530

    Words displaying on screen

--- code/guimoves.cc
+++ code/guimoves.cc
@@ -1,17 +1,37 @@
 #include "guimoves.h"
-#include "SDL_ttf.h"
+#include <iostream>
 
 GUIMoves::GUIMoves(std::shared_ptr<Board> b)
   : GUIElem(b)
-{}
+{
+  if(TTF_Init() != 0) {
+    exit(1);
+  }
+  this->f = TTF_OpenFont("assets/fonts/FreeSerif.ttf", 12);
+}
 
 void GUIMoves::load_assets(SDL_Renderer *r) {
   return;
 }
 
 void GUIMoves::handle(SDL_Renderer *r) {
+  std::cout << "This function is called" << std::endl;
+  SDL_RenderSetViewport(r, viewport);
+  auto text_surface = TTF_RenderText_Solid_Wrapped(f, "Helloaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", { 128, 128, 128, 255 }, 300);
+  auto text_texture = SDL_CreateTextureFromSurface(r, text_surface);
+  SDL_Rect rect = {0, 0, viewport->w, viewport->h};
+  SDL_RenderCopy(r, text_texture, NULL, &rect);
+  SDL_RenderPresent(r);
+  return;
+}
+
+void GUIMoves::update(SDL_Renderer *r) {
+  // remove this line
+  this->handle(r);
   return;
 }
 
 GUIMoves::~GUIMoves(void) {
+  TTF_CloseFont(f);
+  TTF_Quit();
 }
--- code/guimoves.h
+++ code/guimoves.h
@@ -2,12 +2,15 @@
 #define GUIMOVES_H_
 
 #include "guielem.h"
+#include "SDL_ttf.h"
 
 class GUIMoves : public GUIElem {
+  TTF_Font *f;
   public:
   GUIMoves(std::shared_ptr<Board> b);
   void load_assets(SDL_Renderer *r) override;
   void handle(SDL_Renderer *r) override;
+  void update(SDL_Renderer *r) override;
   ~GUIMoves(void);
 };
 
